95a83279653b17e77d13ae96d2451da94da8713d
Jackson Myers <jacksonthomyers@gmail.com>
2023-05-24 11:03:07 -0500

    support for dnf (fedora)

--- sway-install.sh
+++ sway-install.sh
@@ -7,5 +7,5 @@ cp .profile ~/.profile
 cp .gtkrc-2.0 ~/.gtkrc-2.0
 chmod -R +x ~/.config/sway/scripts
 chmod -R +x ~/.config/waybar/scripts
-sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
+sudo dnf install $(cat packages-repository.txt)
 dbus-launch dconf load / < xed.dconf

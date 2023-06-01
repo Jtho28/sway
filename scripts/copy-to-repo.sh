3d0b11f9f2e444ae3c4a5b405aa281cd062690f5
Jackson Myers <jacksonthomyers@gmail.com>
2023-06-01 14:00:58 -0500

    Fix dir copy

--- scripts/copy-to-repo.sh
+++ scripts/copy-to-repo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo rm -r .config/sway/*
-sudo rm -r .config/wofi/*
-sudo rm -r .config/waybar/*
-sudo cp -r ~/.config/sway/* .config/sway/
-sudo cp -r ~/.config/wofi/* .config/wofi/
-sudo cp -r ~/.config/waybar/* .config/waybar/
+sudo rm -r ../.config/sway/*
+sudo rm -r ../.config/wofi/*
+sudo rm -r ../.config/waybar/*
+sudo cp -r ~/.config/sway/* ../.config/sway/
+sudo cp -r ~/.config/wofi/* ../.config/wofi/
+sudo cp -r ~/.config/waybar/* ../.config/waybar/
--- scripts/sway-install-arch.sh
+++ scripts/sway-install-arch.sh
@@ -2,9 +2,9 @@
 
 # Easy setup for testing
 
-cp -R .config/* ~/.config/
-cp .profile ~/.profile
-cp .gtkrc-2.0 ~/.gtkrc-2.0
+cp -R ../.config/* ~/.config/
+cp ../.profile ~/.profile
+cp ../.gtkrc-2.0 ~/.gtkrc-2.0
 chmod -R +x ~/.config/sway/scripts
 chmod -R +x ~/.config/waybar/scripts
 sudo pacman -Syu --needed --noconfirm - < packages-arch.txt
